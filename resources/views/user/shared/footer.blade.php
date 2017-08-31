0cf2cf92b905f2c2a086313f8ad3cea7cebe4c7a
hongnhungtran <nhung@i-enter.co.jp>
2017-08-31 19:03:22 +0900

    add layout user
    add layout user

---
+++ resources/views/user/shared/footer.blade.php
@@ -0,0 +1,23 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="footer-top-layout col-md-12">
+				<h4>リニュの世界</h4>
+				<p>何年経っても色褪せないような 大事な大事な一日にしよう もう二度と同じ日は来ないから</p>
+				<div class="post-blog-date">2017年07月03日</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="footer-bottom-layout footer">
+				<div class="copyright-tag">Copyright © 2017 Wonder Girls Company. All Rights Reserved.</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="top-color"></div>
