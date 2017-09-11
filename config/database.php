2554b4b8227d63ad519394f16fef55235374ab9a
hongnhungtran <nhung@i-enter.co.jp>
2017-09-11 09:21:21 +0900

    DB
    DB

--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => null,
         ],
 
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'meow-learning.000webhostapp.com'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'meow-learning'),
+            'username' => env('DB_USERNAME', 'nhung'),
+            'password' => env('DB_PASSWORD', 'kei01051984'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
