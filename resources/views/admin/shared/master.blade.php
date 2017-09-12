bd9f43780191eb01b3f103529f5fa2a7f7bddfa5
hongnhungtran <nhung@i-enter.co.jp>
2017-09-12 11:20:19 +0900

    test
    test

---
+++ resources/views/admin/shared/master.blade.php
@@ -0,0 +1,11 @@
+{{-- resources/views/admin/dashboard.blade.php --}}
+
+@extends('adminlte::page')
+
+@section('css')
+    <!-- <link rel="stylesheet" href="../../public/css/admin_custom.css"> -->
+@stop
+
+@section('js')
+    <!-- <script> console.log('Hi!'); </script> -->
+@stop
