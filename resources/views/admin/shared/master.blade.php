c713e00f2adb05f042cb421a8b2b15a500cd297e
hongnhungtran <nhung@i-enter.co.jp>
2017-09-12 11:15:39 +0900

    change-public-url
    change-public-url

--- resources/views/admin/shared/master.blade.php
+++ resources/views/admin/shared/master.blade.php
@@ -3,7 +3,7 @@
 @extends('adminlte::page')
 
 @section('css')
-    <!-- <link rel="stylesheet" href="/css/admin_custom.css"> -->
+    <!-- <link rel="stylesheet" href="../../public/css/admin_custom.css"> -->
 @stop
 
 @section('js')
